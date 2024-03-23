f2f499a246eb9ce3a1f42db34735f6d470548b0f
Jainil Shah <jainilShah2113@gmail.com>
2024-03-24 03:42:14 +0530

    made and updated rout
    rout

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,8 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Available routes : 
+app.use('/api/auth',require('./routs/auth'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
